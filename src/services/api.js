8671b0e4c4a21290edffcc5d974b15206986de4f
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2021-03-12 14:12:14 +0100

    httpsAgent

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+import https from "https";
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export const api = axios.create({
   baseURL: "http://api.nbp.pl/api/exchangerates/rates/a",
+  httpAgent: agent,
 });
